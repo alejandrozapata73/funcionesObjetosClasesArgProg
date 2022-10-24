4a88749753b7e40e23c41fbc0a305e183e27c837
alejandro <alejandrozapata73@gmail.com>
2022-10-24 10:04:59 -0300

    subiendo parametros y funciones

--- funciones/script.js
+++ funciones/script.js
@@ -1,22 +1,77 @@
 
 
-var productos = [];
-var precios = [];
-var tipo = [];
+// var productos = [];
+// var precios = [];
+// var tipo = [];
 
 
-for(var i=0;i<2;i++){
+// for(var i=0;i<2;i++){
    
-    productos[i] = prompt("ingrese un producto")
-    precios[i] = prompt("ingrese un precio")
-    tipo[i] = prompt("ingrese un tipo")
+//     productos[i] = prompt("ingrese un producto")
+//     precios[i] = prompt("ingrese un precio")
+//     tipo[i] = prompt("ingrese un tipo")
    
+// }
+
+// console.log(productos.length);
+
+// console.log(productos)
+
+// for(let i=0;i<productos.length;i++){
+//     console.log(productos[i]);
+// }
+
+//principios o ventajas DRY (dont repeat yourself)
+
+function nombre(){
+    console.log("saludando");
 }
 
-console.log(productos.length);
+function nombreEdad(nombre,edad){
 
-console.log(productos)
+    console.log("Nombre Usuario: " + nombre);
+    console.log("Edad Usuario: " + edad);
 
-for(let i=0;i<productos.length;i++){
-    console.log(productos[i]);
 }
+
+//invocar, llamar
+
+nombre();
+
+nombreEdad("Maximo",25);
+nombreEdad("Bruno",36);
+
+function sumarNumero(num1,num2=40,num3=16){
+
+    console.log(num3)
+    console.log(num2);
+    //isNaN-> es No un Numero
+    console.log(isNaN(num2));
+
+    if(num2==" "||num2==null || isNaN(num2) || num2 <=0){
+        alert("no hay nada, tenes vacio, o caracter o es menor a cero")
+
+    }else{
+
+        let total = num1 + num2;
+
+        if(total>25){
+            console.log("es mayor a 25");
+        }else{
+            console.log("es menor a 25");
+        }
+        console.log("El total de la suma es: " + total);
+    }
+
+
+  
+
+
+}
+
+sumarNumero(10);
+
+
+
+//un string...
+// alert("hola alert");
