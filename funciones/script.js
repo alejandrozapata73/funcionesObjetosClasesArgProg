edd3eb1a1c33d36dadce8d5658ea48261ae6a70b
alejandro <alejandrozapata73@gmail.com>
2022-10-24 08:44:41 -0300

    subiendo carpeta funciones

---
+++ funciones/script.js
@@ -0,0 +1,16 @@
+
+
+var productos = [];
+var precios = [];
+var tipo = [];
+
+
+for(var i=0;i<3;i++){
+   
+    productos[i] = prompt("ingrese un producto")
+    precios[i] = prompt("ingrese un precio")
+    tipo[i] = prompt("ingrese un tipo")
+   
+}
+
+console.log(productos)
